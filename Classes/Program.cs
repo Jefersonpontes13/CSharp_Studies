7fd118fadfe8132864abf2e0b2d2d0ae5fe0e114
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-16 15:50:03 -0300

    Adiciona Estudos sobre Classe em C#
    Bootcamp GFT .NET #2
    Uso de Classe em C#

---
+++ Classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Classes.Classes;
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto origem = new Ponto(0, 0);
+            Ponto ponto1 = new Ponto(1, 1);
+            Console.WriteLine(origem.x);
+            Console.WriteLine(ponto1.y);
+        }
+    }
+}
