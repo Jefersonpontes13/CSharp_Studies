2216138896b68cd0bcd62b25e0df2547192ebe75
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-16 22:26:19 -0300

    Adiciona estudos sobre Classes e Métodos
    Bootcamp DIO GFT .NET #2
    Estudos sobre declaraçao e uso de classes e métodos

---
+++ ClassesCSharp/Classes/Ponto3D.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes.Classes
+{
+    public class Ponto3D : Ponto
+    {
+        public int Z;
+        public Ponto3D(int x, int y, int z) : base(x, y)
+        {
+            this.Z = z;
+            CalcularDistancia();
+        }
+
+        public static void Calcular()
+        {
+            //Faz alguma coisa...
+        }
+        public override void CalcularDistancia3()
+        {
+            //Faz outra coisa ...
+            base.CalcularDistancia3();
+        }
+    }
+}
--- ClassesCSharp/Program.cs
+++ ClassesCSharp/Program.cs
@@ -10,6 +10,13 @@ namespace Classes
             Ponto ponto1 = new Ponto(1, 1);
             Console.WriteLine(origem.x);
             Console.WriteLine(ponto1.y);
+
+            Ponto3D origem3D = new Ponto3D(0, 0, 0);
+            Ponto ponto2 = new Ponto3D(1, 1, 1);
+            
+            Console.WriteLine(origem3D.x);
+            Console.WriteLine(ponto2.y);
+
         }
     }
 }
