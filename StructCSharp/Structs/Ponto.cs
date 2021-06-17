6bc7521f5533bd84a72dfdc306f4b2fab7d5ed77
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-16 23:11:11 -0300

    Adiciona estudos sobre Structs e Enum em C# e Revisão
    Bootcamp GFT .NET #2
    
    Exemplo de uso de struct e enum

---
+++ StructCSharp/Structs/Ponto.cs
@@ -0,0 +1,13 @@
+﻿namespace StructCSharp.Structs
+{
+    public struct Ponto
+    {
+        public int X, Y;
+
+        public Ponto(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
