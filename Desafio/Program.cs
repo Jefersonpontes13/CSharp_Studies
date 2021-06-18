e0e7b92f5e41442e4b6d9645547d411568437e39
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-18 15:16:39 -0300

    Adiciona desafio
    Bootcamp DIO .NET #2 GFT

--- Desafio/Program.cs
+++ Desafio/Program.cs
@@ -11,6 +11,19 @@ namespace Desafio
             
         }
 
+        private static void SomaDeDoisNumeros()
+        {
+            //Declare Variables
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+        
+            //Insira as variaveis de acordo com o solicitado
+            int SOMA = A + B;
+
+            //Print
+            Console.WriteLine("SOMA = {0}", SOMA);
+        }
+        
         private static void DivisaoDeXNumeros()
         {
             int limit = Int32.Parse(Console.ReadLine());
