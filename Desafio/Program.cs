9496ba1832021df20e3c6c4a13168e7f99386729
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-18 15:15:37 -0300

    Cria projeto de Desafios de lógica
    Bootcamp GFT .NET #2
    Adiciona desafio

---
+++ Desafio/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DivisaoDeXNumeros();
+            
+        }
+
+        private static void DivisaoDeXNumeros()
+        {
+            int limit = Int32.Parse(Console.ReadLine());
+            List<string> resultados = new List<string>();
+            
+            for (int i = 0; i < limit; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+                if (Y == 0)
+                {
+                    resultados.Add("divisao impossivel"); 
+                }
+                else
+                {
+                    resultados.Add((X / Y).ToString("N1"));
+                }
+            }
+
+            foreach (var resultado in resultados)
+            {
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+    
+
+    
+}
+
