04d8adfd708d2031c5d1c03dbce09bca1031212a
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-13 11:04:32 -0300

    Adiciona Soluções de problemas - PreencimentoDeVetor()

---
+++ SolucaoDeProblemas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SolucaoDeProblemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PreenchimentoDeVetorI();
+        }
+
+        public static void PreenchimentoDeVetorI()
+        {
+            int TAMANHO_VETOR = 10;
+            int valorLido = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("N[{0}] = {1}", 0, valorLido);
+
+            for (int indice = 1; indice < TAMANHO_VETOR; indice++)
+            {
+                Console.WriteLine("N[{0}] = {1}", indice, valorLido * 2);
+                valorLido = valorLido * 2;
+            }
+        }
+    }
+}
